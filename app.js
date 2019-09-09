cfa81b6a385320d2c042f169554b5dd7cd384376
Ryan Propst <ryanpropst@gmail.com>
2019-09-09 09:50:58 -0500

    updated comments and arranged functions to flow with README. Still have to add their variables

--- app.js
+++ app.js
@@ -15,6 +15,16 @@ function sayHello() {
   alert("Hello World!");
 }
 
+//function to check age parms of name and age and a conditional to check if the age is less than 21 and display an alert if under 21 with their name.
+
+function checkAge(name, age) {
+  if (age < 21) {
+    alert("Sorry " + name + ", you aren't old enough to view this page!");
+  } else {
+    // Do nothing.
+  }
+}
+
 // creates an array of favourite vegetables and assigns it to the variable favVeg
 let favVeg = [
   "Cavolo Nero",
@@ -29,6 +39,8 @@ let favVeg = [
 for (var i = 0; i < favVeg.length; i++) {
   console.log(favVeg[i]); // Displays each item in 'favVeggies' array.
 }
+
+// variables created with my friends names and fake ages to use for testing with the check age function. These are objects with key value pairs. The Key is the first and the value is second.
 
 var Fritha = {
   name: "Fritha",
@@ -55,21 +67,14 @@ var Veronica = {
   age: 40
 };
 
+// variable initialized with an array of my friend objects created  above
 var myFriends = [Fritha, Chris, Steve, Miriam, Veronica];
 
 for (var i = 0; i < myFriends.length; i++) {
   checkAge(myFriends[i].name, myFriends[i].age);
 }
 
-function checkAge(name, age) {
-  if (age < 21) {
-    alert("Sorry " + name + ", you aren't old enough to view this page!");
-  } else {
-    // Do nothing.
-  }
-}
-
-//
+//created a function to get the length of any word and then using a modulus with 2 determine if the length is even or odd and display a message based on that caculation.
 
 function getLength(anyWord) {
   let wordLength = anyWord.length;
