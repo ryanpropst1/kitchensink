44d78c4353f2f4732215326439f4c3f26bd3df96
Ryaninoz <ryanpropst@gmail.com>
2019-09-13 16:38:40 -0500

    Update app.js
    updated logic for final myFriends call of an array of Objects

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // created a variable and initialized it to my name
 const myFullName = "Ryan Propst";
 
-//create const varialbe for the total number of states in the US - will not change
+//create const variable for the total number of states in the US - will not change
 const UNITED_STATES = 50;
 
 //created a variable to hold the sum of the calculation 5 + 4
@@ -15,15 +15,15 @@ function sayHello() {
   alert("Hello World!");
 }
 
-//function to check age parmeter of name and age and a conditional to check if the age is less than 21 and display an alert if under 21 with their name. /Write a function called checkAge that takes two arguments: one for a name and one for an age. If the age is less than 21, display an alert that says, "Sorry " + name + ", you aren't old enough to view this page!"
+//function to check age parameter of name and age and a conditional to check if the age is less than 21 and display an alert if under 21 with their name. /Write a function called checkAge that takes two arguments: one for a name and one for an age. If the age is less than 21, display an alert that says, "Sorry " + name + ", you aren't old enough to view this page!"
 //
-
+//
 function checkAge(name, age) {
-  for (var i = 0; i < myFriends.length; i++) {
-  console.log("Sorry " + myFriends[i].name + ", you aren't old enough to view this page!");
+  if (age < 21)
+    console.log("Sorry " + name + ", you aren't old enough to view this page!");
 }
 
-checkAge("Charles", 21 );
+checkAge("Charles", 21);
 checkAge("Abby", 27);
 checkAge("James", 18);
 checkAge("John", 17);
@@ -41,47 +41,41 @@ let favVeg = [
 //iterates thru the favVeg array and displays each veg to the console
 for (var i = 0; i < favVeg.length; i++) {
   console.log(favVeg[i]); // Displays each item in 'favVeggies' array.
-};
+}
 
 // variables created with my friends names and fake ages to use for testing with the check age function. These are objects with key value pairs. The Key is the first and the value is second.
+//
 
 let myFriends = [
+  { name: "Chris", age: 28 },
 
-  {
-    name: "Chris",
-    age: 28,
-  },
+  { name: "Steve", age: 19 },
 
-  {
-    name: "Steve",
-  age: 19,
-  },
+  { name: "Veronica", age: 40 },
 
-  {
-    name: "Veronica",
-    age: 40,
-  },
+  { name: "Miram", age: 35 },
 
-  {
-    name: "Miram",
-    age: 35,
-  },
-
-  {
-    name: "Jed",
-    age: 17,
-  },
+  { name: "Jed", age: 17 }
 ];
-
 
 // variable initialized with an array of my friend objects created  above
 // var myFriends = [Chris, Steve, Veronica, Miriam, Jed ];
 
 //call checkAge function with the array myFriends
 
-checkAge(myFriends.name, myFriends.age);
+function callCheckAge(friends) {
+  for (let i = 0; i < friends.length; i++) {
+    checkAge(friends[i].name, friends[i].age);
+  }
+}
 
-//created a function to get the length of any word and then using a modulus with 2 determine if the length is even or odd and display a message based on that caculation.
+callCheckAge(myFriends);
+
+// myFriends.forEach(function(friend)) {
+//   checkAge(friend);
+// }
+
+//created a function to get the length of any word and then using a modulus with 2 determine if the length is even or odd and display a message based on that calculation.
 
 function getLength(anyWord) {
   let wordLength = anyWord.length;
@@ -91,6 +85,6 @@ function getLength(anyWord) {
   } else {
     console.log("The world is an odd place!"); // Odd.
   }
-};
+}
 
-getLength("Hello World")
+getLength("Hello World");
